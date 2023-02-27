fe7f7c185e27d5b3fc967b92c979806ca5581694
Odile078 <ouwimpuhwe620@daviscollege.com>
2023-02-27 08:59:51 +0200

    chore: Project setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+// pages/_app.js
+import localFont from "@next/font/local";
+
+// Font files can be colocated inside of `pages`
+const nimbus = localFont({
+  src: [
+    {
+      path: "../public/fonts/nibus-sans-bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-nimbus",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${nimbus.variable} font-sans `}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
